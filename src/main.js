29bd3e77f77c869928086ccf50075c7bfffdfaee
Carlos Mejia <mrcarlitos007@gmail.com>
2023-05-14 17:43:46 -0500

    feat: variables and constants

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+let name = 'Pepe';
+name = 'Andres';
+
+const lastname = 'Doe';
+// lastname = 'Roe'; // Error constant
+let condition = true;
+
+if(condition) {
+    const lastname = 'Ale';
+    console.log(lastname)
+}
+console.log("Hello World!!! " + name + " ", + lastname); 
