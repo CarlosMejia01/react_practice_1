30e54571508a513ffe8da29e37cd9d9528310995
Carlos Mejia <mrcarlitos007@gmail.com>
2023-05-26 12:17:44 -0500

    feat: proptypes validation properties

--- 02-intro-react/src/components/HelloWorld.jsx
+++ 02-intro-react/src/components/HelloWorld.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 //prop, deafult value
 export const HelloWorld = ({ user, id, title = "Hello World" }) => {
     console.log(title);
@@ -10,3 +12,10 @@ export const HelloWorld = ({ user, id, title = "Hello World" }) => {
         </>
     )
 };
+
+//proptypes
+HelloWorld.propTypes = {
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    user: PropTypes.object.isRequired,
+}
--- 02-intro-react/src/main.jsx
+++ 02-intro-react/src/main.jsx
@@ -4,6 +4,10 @@ import { HelloWorld } from './components/HelloWorld';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorld user={{name: 'Pepe', lastname: 'Doe'}} id={1} />
+    <HelloWorld
+      user={{ name: 'Pepe', lastname: 'Doe' }}
+      id={1}
+      title='Hello World'
+      />
   </React.StrictMode>,
 )
