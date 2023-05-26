66bfcb89fcd58b8d5a4409a5cafd3f935e9deff5
Carlos Mejia <mrcarlitos007@gmail.com>
2023-05-26 10:53:26 -0500

    feat: first component and fragment

---
+++ 02-intro-react/src/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+export function HelloWorld() {
+    return (
+        //Fragment-dont show in the DOM
+        <>
+            <div>Hello World!</div>
+            <div>Hello World! Two</div>
+        </>
+    )
+}
--- 02-intro-react/src/main.jsx
+++ 02-intro-react/src/main.jsx
@@ -1,30 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { HelloWorld } from './components/HelloWorld';
 
-// const h1 = React.createElement('div', null, React.createElement('ul', null, React.createElement('li', null, 'item 1')));
-// const h1 = <div><ul><li>Hello World</li></ul></div>
-const ul = /*#__PURE__*/ React.createElement(
-  "container",
-  null,
-  /*#__PURE__*/ React.createElement(
-    "div",
-    null,
-    /*#__PURE__*/ React.createElement(
-      "ul",
-      null,
-      /*#__PURE__*/ React.createElement("li", null, "1"),
-      /*#__PURE__*/ React.createElement("li", null, "2"),
-      /*#__PURE__*/ React.createElement("li", null, "3"),
-      /*#__PURE__*/ React.createElement("li", null, "4"),
-      /*#__PURE__*/ React.createElement("li", null, "5")
-    )
-  )
-);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  ul
+  <React.StrictMode>
+     <HelloWorld/>
+  </React.StrictMode>,
 )
