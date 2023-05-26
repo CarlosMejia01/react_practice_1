47aaa457c801ed602636c4c62487443e5eed2b5c
Carlos Mejia <mrcarlitos007@gmail.com>
2023-05-26 11:22:04 -0500

    feat: variables and properties of a component

--- 02-intro-react/src/components/HelloWorld.jsx
+++ 02-intro-react/src/components/HelloWorld.jsx
@@ -1,9 +1,10 @@
-export function HelloWorld() {
+export const HelloWorld = ({ user, id }) => {
+    // const name = 'Pepe';
     return (
         //Fragment-dont show in the DOM
         <>
             <div>Hello World!</div>
-            <div>Hello World! Two</div>
+            <div>Hello World! {user} with id {id}</div>
         </>
     )
 }
--- 02-intro-react/src/main.jsx
+++ 02-intro-react/src/main.jsx
@@ -4,6 +4,6 @@ import { HelloWorld } from './components/HelloWorld';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <HelloWorld/>
+     <HelloWorld user={'Pepe'} id='1'/>
   </React.StrictMode>,
 )
