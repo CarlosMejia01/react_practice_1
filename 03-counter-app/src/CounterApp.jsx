210dbff24ada166c5b965770eae1ade9e84590d3
Carlos Mejia <mrcarlitos007@gmail.com>
2023-05-26 19:47:30 -0500

    feat: counter component

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -1,4 +1,11 @@
-export const CounterApp = () => (
-  <>
+export const CounterApp = () => {
+  let counter = 0;
+  const counterIncrement = () => {
+    counter++;
+    console.log('click!' + counter);
+  }
+  return <>
+    <h2>The counter value is { counter }</h2>
+    <button onClick={() => { counterIncrement() }} >Increment counter +1</button>
   </>
-)
+}
