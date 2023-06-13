0acb31c6128a2905aa1cd741c4e5ae08d89f43b3
Carlos Mejia <mrcarlitos007@gmail.com>
2023-06-12 21:32:52 -0500

    feat: mockup component and destructuring invoice data

--- 04-invoice-app/src/components/InvoiceApp.jsx
+++ 04-invoice-app/src/components/InvoiceApp.jsx
@@ -1,14 +1,29 @@
 import { getInvoice } from "../services/getInvoice";
 
 export const InvoiceApp = () => {
-    const invoice = getInvoice();
+    const { id, name, client, company, items } = getInvoice();
+    const { name: nameClient, lastName, address } = client;
+    const {  country, city, street, number  } = address;
     return (
         <>
-        <h1>Example Invoice</h1>
-        <ul>
-            <li>Id: {invoice.id}</li>
-            <li>Name: {invoice.name}</li>
-        </ul>
+            <h1>Example Invoice</h1>
+            <ul>
+                <li>Id: {id}</li>
+                <li>Name: {name}</li>
+            </ul>
+            <h3>Datos del cliente</h3>
+            <ul>
+                <li>{nameClient} {lastName}</li>
+                <li>{country}</li>
+                <li>{city}</li>
+                <li>{street} {number}</li>
+            </ul>
+            <h3>Company data</h3>
+            <ul>
+                <li>{ company.name }</li>
+                <li>{ company.fiscalNumber }</li>
+            </ul>
+            <h4>Invoice products</h4>
         </>
     )
 };
