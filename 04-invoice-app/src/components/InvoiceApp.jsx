75171d1f6819e6a0c9cbc4c915acd4e644d8156b
Carlos Mejia <mrcarlitos007@gmail.com>
2023-06-12 23:47:00 -0500

    feat: map items to a table

--- 04-invoice-app/src/components/InvoiceApp.jsx
+++ 04-invoice-app/src/components/InvoiceApp.jsx
@@ -3,7 +3,7 @@ import { getInvoice } from "../services/getInvoice";
 export const InvoiceApp = () => {
     const { id, name, client, company, items } = getInvoice();
     const { name: nameClient, lastName, address } = client;
-    const {  country, city, street, number  } = address;
+    const { country, city, street, number } = address;
     return (
         <>
             <h1>Example Invoice</h1>
@@ -20,10 +20,28 @@ export const InvoiceApp = () => {
             </ul>
             <h3>Company data</h3>
             <ul>
-                <li>{ company.name }</li>
-                <li>{ company.fiscalNumber }</li>
+                <li>{company.name}</li>
+                <li>{company.fiscalNumber}</li>
             </ul>
             <h4>Invoice products</h4>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map(({ id, product, price, quantity }) => (<tr key={id}>
+                            <td>{product}</td>
+                            <td>{price}</td>
+                            <td>{quantity}</td>
+                        </tr>
+                    )
+                    )}
+                </tbody>
+            </table>
         </>
     )
 };
