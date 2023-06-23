ca107c5cee859597511f571d56740e5cc791734a
Carlos Mejia <mrcarlitos007@gmail.com>
2023-06-22 20:36:31 -0500

    refactor: file InvoiceApp moved to src and destructuring addres from ClientView

--- 04-invoice-app/src/InvoiceApp.jsx
+++ 04-invoice-app/src/InvoiceApp.jsx
@@ -1,8 +1,8 @@
-import { getInvoice } from "../services/getInvoice";
-import { ClientView } from "./ClientView";
-import { CompanyView } from "./CompanyView";
-import { InvocieView } from "./InvoiceView";
-import { ListItemsView } from "./ListItemsView";
+import { getInvoice } from "./services/getInvoice";
+import { ClientView } from "./components/ClientView";
+import { CompanyView } from "./components/CompanyView";
+import { InvocieView } from "./components/InvoiceView";
+import { ListItemsView } from "./components/ListItemsView";
 
 export const InvoiceApp = () => {
     const { id, name, client, company, items } = getInvoice();
