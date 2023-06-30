a362cd6da0f153125192e3d6e8dd8b5cd964a187
Carlos Mejia <mrcarlitos007@gmail.com>
2023-06-30 09:52:28 -0500

    feat: local data of some products

---
+++ 05-cart-app/src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Mechanical keyboard RGB',
+        description: 'Mechanical keyboard with rgb lights cherry red switches',
+        price: '1000',
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV 55',
+        description: 'Tv LCD ...',
+        price: '3000',
+    },
+    {
+        id: 3,
+        name: 'Headset Bluetooth Sony',
+        description: 'Headset Bluetooth Sony for smarthpones',
+        price: '770',
+    },
+    {
+        id: 4,
+        name: 'Memory Corsair 8GB DDR5',
+        description: 'Memory Optimized for games',
+        price: '3700',
+    },
+    {
+        id: 5,
+        name: 'Asus Nvidia RTX',
+        description: 'Graphic Card for games in 4k resolution',
+        price: '5000',
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core i5',
+        description: 'Optimized for multicore task',
+        price: '4000',
+    },
+]
