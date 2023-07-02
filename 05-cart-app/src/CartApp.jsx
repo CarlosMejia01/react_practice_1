ba011fbbbd8d4d71573a999546da4070af6a47e1
Carlos Mejia <mrcarlitos007@gmail.com>
2023-07-02 17:53:04 -0500

    feat: component cart routes

--- 05-cart-app/src/CartApp.jsx
+++ 05-cart-app/src/CartApp.jsx
@@ -1,38 +1,19 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { CartView } from "./components/CartView";
-import { CatalogView } from "./components/CatalogView";
 import { Navbar } from "./components/Navbar";
+import { CartRoutes } from "./routes/CartRoutes";
 import { useItemsCart } from "./hooks/useItemsCart";
 
 export const CartApp = () => {
     const { cartItems, handlerAddProductCart, handlerDeleteProductCart } = useItemsCart();
     return (
         <>
-        <Navbar />
+            <Navbar />
             <div className="container my-4">
                 <h1>Cart App</h1>
-                <Routes>
-                    <Route
-                        path="catalog"
-                        element={<CatalogView handler={handlerAddProductCart} />}
-                    />
-                    <Route
-                        path="cart"
-                        element={(cartItems?.length <= 0 ?
-                            <div className="alert alert-warning">There are no products in the shopping cart!</div>
-                            :
-                            (
-                                <div className="my-4 w-50">
-                                    <CartView items={cartItems} handlerDelete={handlerDeleteProductCart} />
-                                </div>
-                            ))}
-                    />
-                    <Route
-                        path="/"
-                        element={<Navigate to={'/catalog'} />}
-                    />
-                </Routes>
-                { }
+                <CartRoutes
+                    cartItems={cartItems}
+                    handlerAddProductCart={handlerAddProductCart}
+                    handlerDeleteProductCart={handlerDeleteProductCart}
+                />
             </div>
         </>
     )
