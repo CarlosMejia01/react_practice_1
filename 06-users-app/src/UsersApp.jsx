88bec8bffad0134b46afad7c181395e61cd9b9da
Carlos Mejia <mrcarlitos007@gmail.com>
2023-07-03 14:28:44 -0500

    feat:modal window is added to the user form

--- 06-users-app/src/UsersApp.jsx
+++ 06-users-app/src/UsersApp.jsx
@@ -17,32 +17,48 @@ export const UsersApp = () => {
     } = useUsers();
 
     return (
-        <div className="container my-4">
-            <h2>Users App</h2>
-            <div className="row">
-                {!visibleForm || <div className="col">
-                    <UsersForm
-                        handlerAddUser={handlerAddUser}
-                        initialUserForm={initialUserForm}
-                        userSelected={userSelected}
-                        handlerCloseForm={handlerCloseForm}
-                    /></div>}
-                <div className="col">
-                    {visibleForm || <button
-                        className="btn btn-primary my-2"
-                        onClick={handlerOpenForm}>
-                        New user
-                    </button>}
-                    {users.length === 0
-                        ? <div className="alert alert-warning">No users in the system!</div>
-                        : <UsersList
-                            users={users}
-                            handlerRemoveUser={handlerRemoveUser}
-                            handlerUserSelectedForm={handlerUserSelectedForm}
-                        />
-                    }
+        <>
+            {!visibleForm || <div className="open-modal animation fadeIn">
+                <div className="modal" style={{display: 'block'}} tabIndex="-1">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">
+                                    {userSelected.id > 0 ? 'Edit' : 'Create'} modal users
+                                </h5>
+                            </div>
+                            <div className="modal-body">
+                                <UsersForm
+                                    handlerAddUser={handlerAddUser}
+                                    initialUserForm={initialUserForm}
+                                    userSelected={userSelected}
+                                    handlerCloseForm={handlerCloseForm}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div >
+            </div>}
+            <div className="container my-4">
+                <h2>Users App</h2>
+                <div className="row">
+                    <div className="col">
+                        {visibleForm || <button
+                            className="btn btn-primary my-2"
+                            onClick={handlerOpenForm}>
+                            New user
+                        </button>}
+                        {users.length === 0
+                            ? <div className="alert alert-warning">No users in the system!</div>
+                            : <UsersList
+                                users={users}
+                                handlerRemoveUser={handlerRemoveUser}
+                                handlerUserSelectedForm={handlerUserSelectedForm}
+                            />
+                        }
+                    </div>
+                </div>
+            </div >
+        </>
     )
 }
