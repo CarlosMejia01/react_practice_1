7ab0b5695fb10d274c540773af82440b8b9a8130
Carlos Mejia <mrcarlitos007@gmail.com>
2023-07-03 19:53:54 -0500

    feat: custom hook auth

--- 06-users-app/src/UsersApp.jsx
+++ 06-users-app/src/UsersApp.jsx
@@ -1,44 +1,14 @@
-import { useReducer } from "react"
 import { LoginPage } from "./auth/pages/LoginPage"
-import { loginReducer } from "./auth/pages/reducers/LoginReducer"
 import { UsersPage } from "./pages/UsersPage"
-import Swal from "sweetalert2"
 import { Navbar } from "./components/layout/Navbar"
+import { useAuth } from "./auth/hooks/useAuth"
 
-const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
-    isAuth: false,
-    user: undefined,
-}
 export const UsersApp = () => {
-    const [login, dispatch] = useReducer(loginReducer, initialLogin)
-
-    const handlerLogin = ({ username, password }) => {
-        if (username === 'admin' && password === '12345') {
-            const user = { username: 'admin' };
-            dispatch({
-                type: 'login',
-                payload: user,
-            });
-            sessionStorage.setItem('login', JSON.stringify({
-                isAuth: true,
-                user
-            }));
-        } else {
-            Swal.fire(
-                'Login error',
-                'Invalid username and password',
-                'error'
-            );
-        }
-    };
-
-    const handlerLogout = () => {
-        dispatch({
-            type: 'logout',
-        });
-        sessionStorage.removeItem('login');
-    };
-
+    const {
+        login,
+        handlerLogin,
+        handlerLogout
+    } = useAuth();
     return (
         <>
             {login.isAuth
